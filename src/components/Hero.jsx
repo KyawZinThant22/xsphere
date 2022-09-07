1c1563a138e42f782c5f55dc683f844520adcd8d
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2022-09-07 16:19:58 +0630

    to merge

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,20 @@
 import { BsFillEyeFill } from "react-icons/bs";
-import sign from "../assets/sign.png";
+import sign from "../assets/hand.gif";
 
 const instruction =
   "These are the questions corresponding to the template you have selected, you can modifiy, add or delete them.";
 
 const Hero = () => {
   return (
-    <div className="grid place-items-center space-y-4 mt-[8rem]">
-      <img src={sign} alt="sign" className="w-90" />
+    <div className="grid place-items-center">
+      <img src={sign} alt="sign" className="w-48" />
       <div className="flex items-center gap-3  mt-12">
         <h1 className="text-4xl font-semibold">Question list</h1>
         <div className="bg-gray-400 rounded-full p-1 px-2 text-[10px] text-white">
           12
         </div>
       </div>
-      <p className="w-[33rem] leading-[1.8rem] text-[18px] text-gray-500">
+      <p className="w-[33rem] leading-[1.8rem] text-[18px] text-gray-500 text-center">
         {instruction}
       </p>
 
