b7e83d98ffd60a7e65681266a477b1b41f723323
KyawZinThant22 <kyawthantver2@gmail.com>
2022-09-07 16:23:00 +0630

    fixed right nav and left bar

--- src/pages/QuestionListPage.jsx
+++ src/pages/QuestionListPage.jsx
@@ -4,7 +4,7 @@ import QuestionList from "../components/QuestionList/QuestionList";
 
 const QuestionListPage = () => {
   return (
-    <div>
+    <div className="w-full h-full overflow-y-scroll">
       <Hero />
       <div className="mt-12 grid place-items-center ">
         <QuestionList />
