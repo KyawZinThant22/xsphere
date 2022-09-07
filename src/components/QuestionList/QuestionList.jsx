84327b79c49d6b84ff5cf4b9f3d8f35c3060319c
KyawZinThant22 <kyawthantver2@gmail.com>
2022-09-07 16:51:36 +0630

    question list dnd

--- src/components/QuestionList/QuestionList.jsx
+++ src/components/QuestionList/QuestionList.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react";
 import QuestionDetails from "./QuestionDetails";
+import { ReactSortable } from "react-sortablejs";
 
 const questiondata = [
   {
@@ -21,38 +22,24 @@ const questiondata = [
 
 const QuestionList = () => {
   const [data, setData] = useState(questiondata);
-  const dragItem = useRef();
-  const dragOverItem = useRef();
-  const dragStart = (e, position) => {
-    dragItem.current = position;
-  };
-  const dragEnter = (e, position) => {
-    dragOverItem.current = position;
-  };
 
-  const drop = (e) => {
-    const copyListItems = [...data];
-    const dragItemContent = copyListItems[dragItem.current];
-    copyListItems.splice(dragItem.current, 1);
-    copyListItems.splice(dragOverItem.current, 0, dragItemContent);
-    dragItem.current = null;
-    dragOverItem.current = null;
-    setData(copyListItems);
-  };
   return (
-    <div className=" space-y-6  mb-12 col-span-6   ">
+    <ReactSortable
+      group="groupName"
+      animation={200}
+      VdelayOnTouchStart={true}
+      ghostClass="ghost"
+      delay={2}
+      list={data}
+      setList={setData}
+      className=" space-y-6  mb-12 col-span-6   "
+    >
       {data.map((data, index) => (
-        <div
-          draggable
-          onDragStart={(e) => dragStart(e, index)}
-          onDragEnter={(e) => dragEnter(e, index)}
-          onDragEnd={drop}
-          key={index}
-        >
+        <div>
           <QuestionDetails data={data} />
         </div>
       ))}
-    </div>
+    </ReactSortable>
   );
 };
 
