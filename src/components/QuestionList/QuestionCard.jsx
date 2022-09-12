28f3bb63acca69d3c495608bd2319f5e2cbc58c9
KyawZinThant22 <kyawthantver2@gmail.com>
2022-09-12 11:04:05 +0630

    questionlist and card ui fix

--- src/components/QuestionList/QuestionCard.jsx
+++ src/components/QuestionList/QuestionCard.jsx
@@ -9,57 +9,70 @@ import QuesFullCard from "./QuesFullCard";
 const QuestionCard = ({ index, data }) => {
   const [visible, setVisible] = useState(false);
   // Variables
-  const icons = (
-    <div className="flex flex-col space-y-2 justify-center items-center">
-      <MdDragIndicator
-        className="text-xl text-iconGray"
-        onClick={() => setVisible(!visible)}
-      />
-      {visible && <IoMdAddCircleOutline className="text-lg text-iconGray" />}
-      {visible && <BiMinusCircle className="text-lg text-iconGray" />}
-    </div>
-  );
-  const miniNavbar = (
-    <div className="text-[#77808F] flex felx-row justify-between items-center mb-3">
-      <div className="flex flex-row text-sm space-x-1">
-        <span>Asked to:</span>
-        <h4 className="flex flex-row items-center space-x-2 font-medium ">
-          <span>Reviewee</span> <FaUserCircle />
-        </h4>
-      </div>
-      <div>
-        <span className="text-sm font-medium">
-          Text Answer <span className="text-lg font-semibold">A</span>
-        </span>
-      </div>
-    </div>
-  );
-  const question = (
-    <div className="flex flex-row items-start space-x-3">
-      <span className="bg-[#2EC39B] w-7 h-[1.4rem] grid place-items-center rounded-full text-white text-sm">
-        {index + 1}
-      </span>
-      <h4 className="font-medium leading-relaxed">{data.question}</h4>
-    </div>
-  );
 
   return (
-    <div className="w-full flex flex-row justify-center items-center gap-x-2 overflow-hidden">
-      {icons}
-      <div className="border-2 px-9 py-8 rounded-lg bg-white cursor-pointer select-none">
-        {miniNavbar}
-        {question}
-        <AnimatePresence>
-          <motion.div
-            key={visible}
-            initial={{ height: 0, opacity: 0 }}
-            animate={{ height: "auto", opacity: 1 }}
-            exit={{ height: 0, opacity: 0 }}
-            transition={{ duration: 1 }}
-            className="overflow-hidden">
-            {visible && <QuesFullCard data={data} />}
-          </motion.div>
-        </AnimatePresence>
+    <div className="flex gap-3 w-full">
+      <div className="flex flex-col space-y-2 justify-center items-center">
+        <MdDragIndicator className="text-xl text-iconGray cursor-pointer" />
+        {visible && <IoMdAddCircleOutline className="text-lg text-iconGray" />}
+        {visible && <BiMinusCircle className="text-lg text-iconGray" />}
+      </div>
+
+      <div
+        className={`${
+          visible ? "border-green" : "border-gray-200"
+        } w-full border-2  border-solid p-4  rounded-lg`}
+      >
+        <div className="w-full  p-6">
+          <div className="text-[#77808F] flex felx-row justify-between items-center mb-3">
+            <div className="flex flex-row text-sm space-x-1">
+              <span>Asked to:</span>
+              <h4 className="flex flex-row items-center space-x-2 font-medium ">
+                <span>Reviewee</span> <FaUserCircle />
+              </h4>
+            </div>
+            <div>
+              <span className="text-sm font-medium">
+                {visible ? (
+                  <div>Multiple Choice</div>
+                ) : (
+                  <p>
+                    Text Answer <span className="text-lg font-semibold">A</span>
+                  </p>
+                )}
+              </span>
+            </div>
+          </div>
+
+          <div className="flex flex-row items-start space-x-3">
+            <span className=" w-7 h-[1.4rem] grid place-items-center rounded-full text-white text-sm bg-green">
+              {index + 1}
+            </span>
+            <h4
+              className="font-medium leading-relaxed cursor-pointer"
+              onClick={() => setVisible(!visible)}
+            >
+              {data.question}
+            </h4>
+          </div>
+        </div>
+
+        {visible && (
+          <div>
+            <AnimatePresence>
+              <motion.div
+                key={visible}
+                initial={{ height: 0, opacity: 0 }}
+                animate={{ height: "auto", opacity: 1 }}
+                exit={{ height: 0, opacity: 0 }}
+                transition={{ duration: 1 }}
+                className="overflow-hidden"
+              >
+                {visible && <QuesFullCard data={data} />}
+              </motion.div>
+            </AnimatePresence>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/pages/QuestionListPage.jsx
+++ src/pages/QuestionListPage.jsx
@@ -8,8 +8,7 @@ const QuestionListPage = () => {
   const [questionList, setQuestionList] = useState([
     {
       id: 0,
-      question:
-        "Do you find yourself struggling more than usual with the current COVID-19 situation?",
+      question: "Do you find yourself struggling ",
     },
     {
       id: 1,
@@ -41,14 +40,15 @@ const QuestionListPage = () => {
           instruction={
             "These are the questions corresponding to the template you have selected, you can modify, add or delete them."
           }
-          title="Question list">
+          title="Question list"
+        >
           <div className="flex items-center gap-2 text-blue-500 mt-4">
             <FaEye size={19} />
             <p className="font-semibold text-[0.85rem]">Preview questions</p>
           </div>
         </Hero>
       </div>
-      <div className="w-11/12 mx-auto mt-16 pb-20">
+      <div className=" mt-16 pb-20">
         <ReactSortable
           animation={200}
           VdelayOnTouchStart={true}
@@ -56,7 +56,8 @@ const QuestionListPage = () => {
           delay={2}
           list={questionList}
           setList={(newValue) => setQuestionList(newValue)}
-          className="grid place-items-center gap-y-3">
+          className="grid place-items-center gap-y-3"
+        >
           {questionList.length > 0 &&
             questionList.map((el) => (
               <QuestionCard key={el.id} index={el.id} data={el} />
